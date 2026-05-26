0a7a6c8f5ada9bbdd66da0b861680453f91e3846
Daniel Zint <daniel.zint@mail.de>
2026-05-26 18:07:10 +0200

    Change geogram link type from PUBLIC to default

--- cmake/geogram.cmake
+++ cmake/geogram.cmake
@@ -69,7 +69,7 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
     		target_link_libraries(OpenMP_TARGET INTERFACE ${OpenMP_CXX_FLAGS})
 	endif()
 
-	target_link_libraries(geogram PUBLIC OpenMP::OpenMP_CXX)
+	target_link_libraries(geogram OpenMP::OpenMP_CXX)
 endif()
 
 ################################################################################
